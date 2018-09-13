9d1f6eb221c416fe7f1c0bf8bc07bd9bc1d2c9ae
jasonromulus <pumcome@me.com>
2018-09-13 01:30:29 -0700

    users see reviews

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
@@ -14,6 +14,15 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// app.get('/', (req, res) => {
+//   res.render('home', { msg: 'Hello World!' });
+// })
+
+let reviews = [
+  { title: "Great Review" },
+  { title: "Next Review" }
+]
+
 app.get('/', (req, res) => {
-  res.render('home', { msg: 'Hello World!' });
+  res.render('reviews-index', { reviews: reviews });
 })
