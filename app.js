1b4fd2694d0a30b56e3fd8e0109fdc5a8eac5c98
jasonromulus <pumcome@me.com>
2018-09-13 12:17:01 -0700

    Users can create reviews

--- app.js
+++ app.js
@@ -19,10 +19,18 @@ app.set('view engine', 'handlebars');
 // })
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/rotten-potatoes',{useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+  console.log('It is connected.')
+});
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection Error:'));
+// mongoose.set('debug', true);
 
 const Review = mongoose.model('Review', {
-  title: String
+  title: String,
+  description: String,
+  movieTitle: String
 });
 
 // let reviews = [
@@ -43,3 +51,31 @@ app.get('/', (req, res) => {
 app.get('/', (req, res) => {
   res.render('reviews-index', { reviews: reviews });
 })
+
+app.get('/reviews/new', (req, res) => {
+  res.render('reviews-new', {});
+})
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/reviews', (req, res) => {
+  console.log(req.body);
+  // res.render('reviews-new', {});
+})
+
+app.post('/reviews', (req, res) => {
+  Review.create(req.body).then((review) => {
+    console.log(review);
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
+
+// const Review = mongoose.model('Review', {
+//   title: String,
+//   description: String,
+//   movieTitle: String
+// });
