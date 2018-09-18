16334a9e106a0ea2833b8a290e1e0962437e2445
jasonromulus <pumcome@me.com>
2018-09-18 10:39:18 -0700

    Users can see single reviews

--- app.js
+++ app.js
@@ -1,31 +1,9 @@
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express()
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-app.listen(3000, () => {
-  console.log('App listening on port 3000!')
-})
-
-var exphbs = require('express-handlebars');
-
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-
-// app.get('/', (req, res) => {
-//   res.render('home', { msg: 'Hello World!' });
-// })
-
 const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/rotten-potatoes',{useNewUrlParser: true });
-mongoose.connection.once('open', () => {
-  console.log('It is connected.')
-});
-mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection Error:'));
-// mongoose.set('debug', true);
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 
 const Review = mongoose.model('Review', {
   title: String,
@@ -33,49 +11,87 @@ const Review = mongoose.model('Review', {
   movieTitle: String
 });
 
-// let reviews = [
-//   { title: "Great Review" },
-//   { title: "Next Review" }
-// ]
+// override with POST having ?_method=DELETE or ?_method=PUT
+app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-  Review.find()
-    .then(reviews => {
-      res.render('reviews-index', { reviews: reviews });
-    })
-    .catch(err => {
-      console.log(err);
-    })
-})
 
-app.get('/', (req, res) => {
-  res.render('reviews-index', { reviews: reviews });
-})
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
-app.get('/reviews/new', (req, res) => {
-  res.render('reviews-new', {});
-})
 
-const bodyParser = require('body-parser');
+mongoose.connect('mongodb://localhost/rotten-potatoes',{useNewUrlParser: true });
+// mongoose.connection.once('open', () => {
+//   console.log('It is connected.');
+// });
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection Error:'));
+// mongoose.set('debug', true);
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/reviews', (req, res) => {
-  console.log(req.body);
-  // res.render('reviews-new', {});
-})
+app.get('/reviews/new', (req, res) => {
+  res.render('reviews-new', {});
+});
 
+
+// INDEX
+app.get('/', (req, res) => {
+  Review.find()
+  .then(reviews => {
+    res.render('reviews-index', { reviews: reviews });
+  })
+  .catch(err => {
+    console.log(err);
+  });
+});
+
+// CREATE
 app.post('/reviews', (req, res) => {
   Review.create(req.body).then((review) => {
     console.log(review);
+    res.redirect(`/reviews/${review._id}`); // Redirect to reviews/:id
+  }).catch((err) => {
+    console.log(err.message)
+  });
+});
+
+// SHOW
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id).then((review) => {
+    res.render('reviews-show', { review: review })
+  }).catch((err) => {
+    console.log(err.message);
+  });
+});
+
+// EDIT
+app.get('/reviews/:id/edit', (req, res) => {
+  Review.findById(req.params.id, function(err, review) {
+    res.render('reviews-edit', {review: review});
+  });
+});
+
+// UPDATE
+app.put('/reviews/:id', (req, res) => {
+  Review.findByIdAndUpdate(req.params.id, req.body)
+    .then(review => {
+      res.redirect(`/reviews/${review._id}`)
+    })
+    .catch(err => {
+      console.log(err.message)
+    });
+});
+
+// DELETE
+app.delete('/reviews/:id', function (req, res) {
+  console.log("DELETE review")
+  Review.findByIdAndRemove(req.params.id).then((review) => {
     res.redirect('/');
   }).catch((err) => {
     console.log(err.message);
-  })
-})
+  });
+});
 
-// const Review = mongoose.model('Review', {
-//   title: String,
-//   description: String,
-//   movieTitle: String
-// });
+app.listen(3000, () => {
+  console.log('App listening on port 3000!')
+});
